b957be9440f415fd0914165b1514749456e91d9c
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-11-30 03:16:23 -0500

    code modified

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ df = pd.read_csv('nato_phonetic_alphabet.csv')
 
 df_dict = { row.letter:row.code for (index, row) in df.iterrows()}
 
-print(df_dict)
+user_input = input("Enter a word: ").upper()
+
+nato_list = [ df_dict[char] for char in user_input if char.isalpha()]
+
+print(nato_list)
